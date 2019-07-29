bba2195507cb80631369e6488c2a0c8645eee0ad
Kate Kim <katekim5809@gmail.com>
2019-07-29 17:48:46 -0400

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,9 @@ Student.create(firstname:"Steve", lastname:"Rogers", email:"steve.r@tea.com", im
 Student.create(firstname:"Logan", lastname:"Howlett", email:"logan.h@tea.com", img: nil, age: 17, grade: 9, gender: "male", guardians_name: "wolverine", relationship_to_student: "dad", guardians_email: "wolverine@tea.com", guardians_phone: "9144395809", inclass: true, demerits: 0)
 
 Student.create(firstname:"Peter", lastname:"Parker", email:"steve.r@tea.com", img: nil, age: 17, grade: 8, gender: "male", guardians_name: "Mary", relationship_to_student: "Aunt", guardians_email: "sarahrogers@tea.com", guardians_phone: "9144395809", inclass: true, demerits: 0)
+
+
+Student.create(firstname:"Arya", lastname:"Stark", email:"aray@tea.com", img: nil, age: 15, grade: 8, gender: "female", guardians_name: "Sansa", relationship_to_student: "Sister", guardians_email: "sansa@tea.com", guardians_phone: "9144395809", inclass: true, demerits: 0)
+
+
+Student.create(firstname:"Bran", lastname:"Stark", email:"bran@tea.com", img: nil, age: 17, grade: 10, gender: "female", guardians_name: "Sansa", relationship_to_student: "Sister", guardians_email: "sansa@tea.com", guardians_phone: "9144395809", inclass: true, demerits: 0)
